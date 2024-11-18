a51b3a7e7203f0e792fe79ef41b8bcbfae71660a
geek_urooj <150442226+uroojzahranaqvi@users.noreply.github.com>
2024-11-18 18:43:32 +0530

    Update operatorAssign.js

--- operatorAssign.js
+++ operatorAssign.js
@@ -64,3 +64,110 @@ console.log(`First number after &&= is : ${firstNumber}`);
 firstNumber ||= secondNumber;
 console.log(`First number after ||= is : ${firstNumber}`);
 
+// Problem 3: Write a JavaScript program to compare two numbers and log whether they are equal, greater, or lesser.
+
+let firstNumber = parseFloat(prompt(`Enter the first number`));
+let secondNumber = parseFloat(prompt(`Enter the second number`));
+
+if(firstNumber === secondNumber){
+    console.log(`Numbers are equal`);
+}
+else if(firstNumber > secondNumber){
+    console.log(`First number is greater than second number`);
+}
+else if(firstNumber < secondNumber){
+    console.log(`First number is less than second number`);
+}
+else{
+    console.log(`Enter a valid number`);
+}
+
+// Problem 4: Write a JavaScript program to demonstrate logical operators in a conditional statement.
+
+let age = parseInt(prompt(`Enter your age`));
+
+if(age < 0){
+    console.log(`Please enter a valid age`);
+}
+else if(age <= 10 && age >= 0 || age >= 60){
+    console.log(`Ticket is free`);
+}
+else{
+    console.log(`You have to pay for the tickets`);
+}
+
+// Problem 5: Write a JavaScript program to perform bitwise operations on two numbers.
+
+let firstNumber = parseInt(prompt(`Enter the first number`));
+let secondNumber = parseInt(prompt(`Enter the second number`));
+
+//Bit-wise Operations:
+
+let firstOperation = firstNumber & secondNumber;
+console.log(`The value of bitwise AND operation is ${firstOperation}`);
+
+let secondOperation = firstNumber | secondNumber;
+console.log(`The value of bitwise OR operation is ${secondOperation}`);
+
+let thirdOperation = firstNumber ^ secondNumber;
+console.log(`The value of bitwise XOR operation is ${thirdOperation}`);
+
+let fourthOperation = ~firstNumber;
+console.log(`The value of bitwise NOT operation is ${fourthOperation}`);
+
+let fifthOperation = firstNumber << secondNumber;
+console.log(`The value of bitwise left shift operation is ${fifthOperation}`);
+
+let sixthOperation = firstNumber >> secondNumber;
+console.log(`The value of bitwise right shift operation is ${sixthOperation}`);
+
+let seventhOperation = firstNumber >>> secondNumber;
+console.log(`The value of bitwise unsigned right shift operation is ${seventhOperation}
+`);
+
+// Problem 6: Write a JavaScript program to use the ternary operator to determine the larger of two numbers.
+
+let firstPersonAge = parseInt(prompt(`Enter first person age`));
+let secondPersonAge = parseInt(prompt(`Enter second person age`));
+
+let greaterAge = firstPersonAge > secondPersonAge
+    ? firstPersonAge : secondPersonAge;
+    
+console.log(`The value of Ternary Operation is ${greaterAge}`);
+
+// Problem 7: Write a JavaScript program to demonstrate type operators on different data types.
+
+let number = 7;
+console.log(`The typeof number is : ${typeof number}`);
+
+let fullName = `Urooj Naqvi`;
+console.log(`The typeof string is : ${typeof fullName}`);
+
+let isMarried = false;
+console.log(`The typeof is boolean is : ${typeof isMarried}`);
+
+const person = {
+    name : `Urooj`,
+    age : 20
+}
+console.log(`The typeof object is : ${typeof person}`);
+
+let arr = [1,2,3];
+console.log(`The typeof array is : ${typeof arr}`);
+
+let container = null;
+console.log(`The typeof null is : ${typeof container}`);
+
+let nothing;
+console.log(`The typeof undefined is : ${typeof nothing}`);
+
+function greetings(){
+    console.log(`Hello`);
+}
+console.log(`The typeof function is : ${typeof greetings}`);
+
+// Problem 8: Write a JavaScript program to use string operators to concatenate two strings.
+
+// Problem 9: Write a JavaScript program to demonstrate the use of the typeof operator on different variables.
+   // This is same as problem - 7
+  
